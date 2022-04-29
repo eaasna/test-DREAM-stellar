cab0f085713cb1f41682c068ea1d880e130baec6
Evelin Aasna <evelia95@redwood.imp.fu-berlin.de>
2022-04-29 15:08:47 +0200

    Single scan over query file

--- metagenome/scripts/distribute_search.py
+++ metagenome/scripts/distribute_search.py
@@ -3,20 +3,20 @@ import pandas as pd
 from Bio import SeqIO
 
 #------------ INPUT ------------ 
-#query_file = "../100kb/queries/rep0_e0.025.fastq"
-query_file = snakemake.input.queries
-#search_file = "../100kb/search/rep0_e0.025.out"
-search_file = snakemake.input.search_out
-
-bins = snakemake.config["ibf_bins"]
-bin_list = list(range(bins))
-bin_list = [str(b).zfill(len(str(bins))) for b in bin_list]
-
 rep = snakemake.wildcards.rep
 er = snakemake.wildcards.er
 
-#------------ OUTPUT ------------ 
-#output_prefix = "rep" + str(rep) + "/queries/"
+# query_file = "../10Mb/rep" + str(rep) + "/queries/e" + str(er) + ".fastq"
+# search_file = "../10Mb/rep" + str(rep) + "/search/e" + str(er) + ".out"
+query_file = snakemake.input.queries
+search_file = snakemake.input.search_out
+
+bins = snakemake.config["ibf_bins"]
+bin_int_list = list(range(bins))
+bin_str_list = [str(b).zfill(len(str(bins))) for b in bin_int_list]
+
+#------------ OUTPUT ------------
+# output_prefix = "../10Mb/rep" + str(rep) + "/queries/"
 output_prefix = snakemake.params.out_prefix
 
 # tidy match files
@@ -30,20 +30,18 @@ matches = matches.replace(r'^\s*$', np.nan, regex=True).dropna() # drop empty ro
 matches = matches.reset_index(drop = True)
 matches["bin_id"] = pd.to_numeric(matches["bin_id"])
 
-# write filtered bin queries into separate files
-queries = list(SeqIO.parse(query_file, "fastq"))
-for bin_id in bin_list:
-    if (bin_id.count("0") == len(bin_id)):
-        bin_int = 0
-    else:
-        bin_int = int(bin_id.lstrip("0"))
+# open all output bin query files 
+bin_query_files = [open((output_prefix + 'bin_{}_e' + str(er) + ".fasta").format(b), 'w') for b in bin_str_list]
 
-    bin_matches = matches[matches["bin_id"]==bin_int]
-    seg_out_file = output_prefix + "bin_" + bin_id + "_e" + str(er) + ".fasta"
-    with open(seg_out_file, "w") as output_handle:
-        for query in queries:
-            if (query.name in list(bin_matches["read_id"])):
-                SeqIO.write(query, output_handle, "fasta")
+queries = list(SeqIO.parse(query_file, "fastq"))
+# scan over query file once
+for query in queries:
+    read_matches = matches[matches["read_id"]==int(query.name)]
+    for index, match in read_matches.iterrows():
+        SeqIO.write(query, bin_query_files[match["bin_id"]], "fasta")
+    
+for f in bin_query_files:
+    f.close()
 
 # remove file if no queries for bin
 # import subprocess
