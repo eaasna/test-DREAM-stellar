788f17b7a189b5b95d6695e8ba1ddce9da411aec
Evelin Aasna <evelin.aasna@fu-berlin.de>
2022-05-08 19:36:46 +0200

    Bin queries in RAM

--- metagenome/run_bin_nr_test.sh
+++ metagenome/run_bin_nr_test.sh
@@ -3,11 +3,12 @@
 size=${1}
 er=0.05
 
-for bins in 64 1024 8192 16384
+#for bins in 64 1024 8192 16384
+for bins in 64
 do
 snakemake --cores 1 --snakefile Snakefile_simulate --configfile $size/config.yaml --config error_rate=$er ibf_bins=$bins --
-( /usr/bin/time -a -o $size/stellar.time -f "%e\t%M\t%x\t%C" snakemake --cores 16 --snakefile Snakefile_stellar --configfile $size/config.yaml --config error_rate=$er ibf_bins=$bins -- ) 2> $size/stellar.err
-( /usr/bin/time -a -o $size/dream.time -f "%e\t%M\t%x\t%C" snakemake --cores 16 --snakefile Snakefile_dream --configfile $size/config.yaml --config error_rate=$er ibf_bins=$bins -- ) 2> $size/dream.err
+( /usr/bin/time -a -o $size/stellar.time -f "%e\t%M\t%x\t%C" snakemake --use-conda --cores 16 --snakefile Snakefile_stellar --configfile $size/config.yaml --config error_rate=$er ibf_bins=$bins -- ) 2> $size/stellar.err
+( /usr/bin/time -a -o $size/dream.time -f "%e\t%M\t%x\t%C" snakemake --use-conda --cores 16 --snakefile Snakefile_dream --configfile $size/config.yaml --config error_rate=$er ibf_bins=$bins -- ) 2> $size/dream.err
 done
 
 # snakemake --cores 1 --snakefile Snakefile_evaluate --configfile $size/config.yaml --config error_rate=$er ibf_bins=$bins --
--- metagenome/run_dream.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-size=${1}
-er=${2}
-
-#( /usr/bin/time -a -o $size/$er\_stellar.time -f "%e\t%M\t%x\t%C" snakemake --cores 16 --snakefile Snakefile_stellar --configfile $size/config.yaml --config error_rate=$er) 2> $size/$er\_stellar.err
-( /usr/bin/time -a -o $size/$er\_dream.time -f "%e\t%M\t%x\t%C" snakemake --cores 16 --snakefile Snakefile_dream --configfile $size/config.yaml --config error_rate=$er) 2> $size/$er\_dream.err
