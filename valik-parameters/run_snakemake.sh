682e1ae86bdab002905c40127ea229716f1a0cf4
Evelin Aasna <evelin.aasna@fu-berlin.de>
2022-05-22 13:02:36 +0200

    Kmers with wrong error rate

--- valik-parameters/run_snakemake.sh
+++ valik-parameters/run_snakemake.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+# (w, k)-minimisers
 snakemake --cores 8 --use-conda --configfile size_1_haplotype/config.yaml
 snakemake --cores 8 --use-conda --configfile size_8_haplotypes/config.yaml
 snakemake --cores 8 --use-conda --configfile overlap/config.yaml
 
+# (k, k)-minimisers
+snakemake --cores 8 --use-conda --configfile size_1_haplotype_kmers/config.yaml
+snakemake --cores 8 --use-conda --configfile size_8_haplotypes_kmers/config.yaml
+snakemake --cores 8 --use-conda --configfile overlap_kmers/config.yaml
