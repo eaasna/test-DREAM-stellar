f463302ffe271ebdd74e742d4890de5c6cf66707
Evelin Aasna <evelin.aasna@fu-berlin.de>
2023-08-09 11:06:18 +0200

    Parameter tuning results (valik v-1)

--- valik-parameters/run_snakemake.sh
+++ valik-parameters/run_snakemake.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # (w, k)-minimisers
-snakemake --cores 16 --configfile size_1_haplotype/config.yaml
-snakemake --cores 16 --configfile size_8_haplotypes/config.yaml
+snakemake --cores 16 --configfile size_1_haplotype/config.yaml --forceall
+snakemake --cores 16 --configfile size_8_haplotypes/config.yaml --forceall
 # snakemake --cores 16 --configfile overlap/config.yaml
 
 # (k, k)-minimisers
-snakemake --cores 16 --configfile size_1_haplotype_kmers/config.yaml
-snakemake --cores 16 --configfile size_8_haplotypes_kmers/config.yaml
+snakemake --cores 16 --configfile size_1_haplotype_kmers/config.yaml --forceall
+snakemake --cores 16 --configfile size_8_haplotypes_kmers/config.yaml --forceall
 # snakemake --cores 16 --configfile overlap_kmers/config.yaml
