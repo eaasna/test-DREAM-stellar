222f8e272e2188ce16574a496413578de98f89e4
Evelin Aasna <evelin.aasna@fu-berlin.de>
2024-03-11 14:42:20 +0100

    Reproduce stellar update

--- reproduce-stellar/run_snakemake.sh
+++ reproduce-stellar/run_snakemake.sh
@@ -5,10 +5,9 @@ set -x
 #snakemake --use-conda --cores 8 --configfile 10kb/config.yaml
 
 #for size in "100kb" "1Mb" "10Mb" "100Mb"
-#runtime is in minutes
 for size in "100Mb"
 do
-	snakemake --forceall --keep-going --cores 16 --configfile ${size}/config.yaml 1> ${size}.out 2> ${size}.err
+	snakemake --forceall --keep-going --cores 32 --configfile ${size}/config.yaml 1> ${size}.out 2> ${size}.err
 done
 
 #python scripts/make_stellar_table2.py
