bc214e0e3aa3bf4a4594ca902076c603a14ee8fe
Evelin Aasna <evelin.aasna@fu-berlin.de>
2024-12-18 09:23:45 +0100

    Kmer spectra neater argument parsing

--- work-python/kmer-spectra/kmer_dump.sh
+++ work-python/kmer-spectra/kmer_dump.sh
@@ -2,10 +2,15 @@
 
 set -x
 
+if [[ "$#" -ne 4 ]]; then
+	echo "Usage: bash kmer_dump.sh <data_dir> <in.fasta> <out.kmer.dump> <k>"
+	exit
+fi	
+
 DIR=$1
 INPUT=$2 # .fasta
 OUTPUT=$3 # kmer.dump
-k=21
+k=$4
 
 cd $DIR
 mkdir -p data
@@ -15,3 +20,4 @@ export PATH="/group/ag_abi/evelina/decode/bin:$PATH"
 kmc_file=$INPUT.res
 kmc -k$k -fm -ci1 -cs64000 -t8 $INPUT $kmc_file data/
 kmc_dump -ci1 $kmc_file $OUTPUT
+
