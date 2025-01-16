07cb1cfbfbe0985826317d9b742d6693d62bac18
Evelin Aasna <evelin.aasna@fu-berlin.de>
2025-01-16 14:40:12 +0100

    Mask repeats

---
+++ structural-variants/mask_repeats.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -e
+
+execs=(dust st_dna5todna4)
+for exec in "${execs[@]}"; do
+    if ! which ${exec} &>/dev/null; then
+        echo "${exec} is not available"
+        echo ""
+        echo "make sure \"${execs[@]}\" are reachable via the \${PATH} variable"
+        echo ""
+
+        echo "try "
+	echo 'export PATH="/group/ag_abi/evelina/seqan3_tools/build/bin:/group/ag_abi/evelina/meme/bin:/group/ag_abi/evelina/meme/libexec/meme-5.5.6:$PATH"'
+
+        exit 127
+    fi
+done
+
+work_dir="/buffer/ag_abi/evelina/1000genomes/hifi/ftp.sra.ebi.ac.uk/vol1/run"
+for sample in $work_dir/*/*/unmapped.fa;do
+	sample_dir="$(dirname "$sample")"
+	
+	masked="$sample_dir/masked.fa"
+	echo "Processing $sample"
+	if [ ! -f $masked ]; then
+		dust $sample > $masked
+	fi
+
+	seq_len=$(grep -v ">" $masked | wc -c | awk '{print $1}')
+	repeat_len=$(grep -v ">" $masked | grep -o N | wc -l | awk '{print $1}')
+
+	echo "Total sequence length $seq_len"
+	echo "Repeat length $repeat_len"
+	frac=$(bc <<< "scale=3; $repeat_len/$seq_len")
+	echo "Fraction of repeats $frac"
+
+	dna4="$sample_dir/dna4.fa"
+	if [ ! -f $dna4 ]; then
+		st_dna5todna4 $masked > $dna4	
+	fi
+done
+
--- structural-variants/run_mapping.sh
+++ structural-variants/run_mapping.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 # conda activate python27
 
-work_dir="/buffer/ag_abi/evelina/1000genomes/hifi/ftp.sra.ebi.ac.uk/vol1/run/ERR386/ERR3861390/"
-reads="$work_dir/HG00732-hifi-r54329U_20190607_183639-D01.bam"
 ref="/srv/data/evelina/human/ref_concat.fa"
 index="/srv/data/evelina/human/ref.mmi"
 
-echo "index $ref"
-pbmm2 index $ref $index --preset HIFI
+#echo "index $ref"
+#pbmm2 index $ref $index --preset HIFI
 
-mapped="$work_dir/pbmm2.bam"	
-unmapped="$work_dir/unmapped.bam"	
+work_dir="/buffer/ag_abi/evelina/1000genomes/hifi/ftp.sra.ebi.ac.uk/vol1/run"
 
-# alignments are discarded if they do not have at least 70% gap-compressed identity
-# https://github.com/PacificBiosciences/pbmm2/blob/develop/README.md#how-do-you-define-gap-compressed-identity 
+for sample in $work_dir/*/*/*1.bam;do
+	sample_dir="$(dirname "$sample")"
+	
+	reads="$sample"
+	mapped="$sample_dir/pbmm2.bam"	
+	unmapped="$sample_dir/unmapped.bam"	
+	fasta="$sample_dir/unmapped.fa"
+	if [ ! -f $mapped ]; then
+		# alignments are discarded if they do not have at least 70% gap-compressed identity
+		# https://github.com/PacificBiosciences/pbmm2/blob/develop/README.md#how-do-you-define-gap-compressed-identity
+		echo "align $reads"
+		pbmm2 align $index $reads $mapped --preset HIFI --unmapped
 
-echo "align $reads"
-pbmm2 align $index $reads $mapped --preset HIFI --unmapped
-
-echo "filter out unmapped"
-samtools view --bam -f 4 $mapped > $unmapped
-
-echo "get fasta"
+		echo "filter out unmapped"
+		samtools view --bam -f 4 $mapped > $unmapped
+	fi
+	
+	echo "make fasta"
+	samtools view $unmapped | awk '{print ">"$1 "\t" $10 }' | sed 's/[ \t]\{1,\}/\n/' > $fasta
+done
 
