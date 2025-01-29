36758016da99ff62b5dfa0292cacddf5f23c7ca6
Evelin Aasna <evelin.aasna@fu-berlin.de>
2025-01-29 11:01:09 +0100

    LASTZ default search

--- work-python/missed-matches/compare_fn_count_tables.sh
+++ work-python/missed-matches/compare_fn_count_tables.sh
@@ -2,12 +2,11 @@
 
 set -ex 
 
-data_dir="/group/ag_abi/evelina/DREAM-stellar-benchmark/genome-wise/work/last"
 stellar="l50_e2"
 min_len=50
 min_percid=96
 
-function count_matches() {
+function count_last_matches() {
 	m=$1
 	fn_gff="$data_dir/$stellar/mouse_vs_fly_w1_k1_m$m.fn.gff"
 	all_matches="$data_dir/mouse_vs_fly_w1_k1_m$m.tsv "
@@ -19,5 +18,21 @@ function count_matches() {
 	grep -v "#" $all_matches | awk -v l="$min_len" -v p="$min_percid" '{ if ( $4 < l && $3 < p ) print $2 "\t" $1}' | sort | uniq -c | awk '{print $1 "\t" $2 "\t" $3}' > $out_dir/fp_count_table.tsv
 }
 
-count_matches 100
-count_matches 10
+
+function count_lastz_matches {
+	fn_gff="$data_dir/$stellar/mouse_vs_fly_s111101001100101011111_gapped_notransition_3.fn.gff"
+	all_matches="$data_dir/mouse_vs_fly_s111101001100101011111_gapped_notransition_3.tsv"
+	out_dir="lastz_vs_stellar_$stellar"
+	mkdir -p $out_dir
+
+	awk '{print $1 "\t" $9}' $fn_gff  | awk -F';' '{print $1}' | sort | uniq -c | awk '{print $1 "\t" $2 "\t" $3}' > $out_dir/fn_count_table.tsv
+
+	grep -v "#" $all_matches | awk -v l="$min_len" -v p="$min_percid" '{ if ( $4 < l && $3 < p ) print $2 "\t" $1}' | sort | uniq -c | awk '{print $1 "\t" $2 "\t" $3}' > $out_dir/fp_count_table.tsv
+}
+
+#data_dir="/group/ag_abi/evelina/DREAM-stellar-benchmark/genome-wise/work/last"
+#count_last_matches 100
+#count_last_matches 10
+
+data_dir="/group/ag_abi/evelina/DREAM-stellar-benchmark/genome-wise/work/lastz_step3"
+count_lastz_matches
